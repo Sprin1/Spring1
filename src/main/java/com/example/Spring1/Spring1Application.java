1e4c5ecfcb4f57c60e9f779846be24e5e7e2fa7b
JhonatanChavarroArias <jchavarro4@misena.edu.co>
2022-08-23 15:55:46 -0500

    Primer commit Jhonatan

---
+++ src/main/java/com/example/Spring1/Spring1Application.java
@@ -0,0 +1,15 @@
+package com.example.Spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1Application.class, args);
+
+		System.out.println("Primer codigo y commit de:\nJhonatan Chavarro Arias\n");
+
+	}
+}
